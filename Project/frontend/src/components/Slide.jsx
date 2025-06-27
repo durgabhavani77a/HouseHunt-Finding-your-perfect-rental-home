92cfa324843233a296e2481a87763451681f5210
Durga Bhavani <durgabhavani77a@gmail.com>
2025-06-28 01:17:09 +0530

    Fixed frontend folder - removed submodule and added actual files

---
+++ Project/frontend/src/components/Slide.jsx
@@ -0,0 +1,13 @@
+import "../styles/Slide.scss"
+
+const Slide = () => {
+  return (
+    <div className="slide">
+      <h1>
+      Home is Wherever You Are! Roam Freely, Stay Present, Make Memories.
+      </h1>
+    </div>
+  );
+};
+
+export default Slide;
